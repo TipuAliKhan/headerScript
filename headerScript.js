431b38cd32dc554e0ec59957c0f847dfbe80f996
Tipu <tipualikhan@gmail.com>
2020-04-16 02:03:43 +0530

    Update headerScript.js

--- headerScript.js
+++ headerScript.js
@@ -1,98 +1,233 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   addNfHeader();
 });
 function addNfHeader(event) {
 
   var template = `<style>
-        body {
-            margin: 0;
-        padding: 0;
-        font-family: 'Open Sans';
-        }
-        
-header.nf-plugin-header {
-            background: #777474;
-        margin: 0;
-        height: 40px;
-        position: fixed;
-        width: 100%;
-        top: 0;
-        z-index: 10000001;
-        }
-        
-.nf-plugin-header ul {
-            padding: 0 30px;
-        margin: 0;
-        list-style: none;
-        line-height: 40px;
-        }
-        
-.nf-plugin-header ul li {
-            display: inline-block;
-        margin: 0 20px;
-        }
-        
-.nf-plugin-header ul li a {
-            color: #fff;
-        text-decoration: none;
-        }
-        
-.popup iframe {
-            width: 98%;
-        height: 100vh;
-        margin: 0 auto;
-        }
-        
-.wrapper {
-            position: relative;
-        margin-top: 40px;
-        }
-        
-a.close {
-            position: absolute;
-        top: 0;
-        right: 5px;
-        text-decoration: none;
-        color: #333;
-        border-radius: 50%;
-        border: 1px solid #333;
-        padding: 0 6px;
-        z-index: 99999;
-        }
+  :root {
+    --bg-color: #4a4a4a;
+    --text-color: #ffffff;
+    --primary-color: #ffb900;
+  }
+  body{
+    margin:0;
+  }
+  #nf-header {
+    color: #ffffff;
+    background-color: #4a4a4a;
+    padding: 6px;
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+            flex-direction: row;
+    -webkit-box-pack: center;
+        -ms-flex-pack: center;
+            justify-content: center;
+    -webkit-box-align: center;
+        -ms-flex-align: center;
+            align-items: center;
+    font-size: 16px;
+  }
+  
+  #nf-header a:hover {
+    color: #ffb900;
+    text-decoration: underline;
+  }
+  
+  #nf-header * {
+    color: #ffffff;
+    background-color: #4a4a4a;
+    text-decoration: none;
+    list-style: none;
+    border: none;
+  }
+  
+  #nf-header .content {
+    width: 90%;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+            flex-direction: row;
+    -ms-flex-pack: distribute;
+        justify-content: space-around;
+    -webkit-box-align: center;
+        -ms-flex-align: center;
+            align-items: center;
+  }
+  
+  #nf-header .content .logo img {
+    vertical-align: bottom;
+    width: 18px;
+    height: 18px;
+    padding-right: 8px;
+  }
+  
+  #nf-header .content nav ul {
+    margin: 0;
+    padding: 0;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+            flex-direction: row;
+  }
+  
+  #nf-header .content nav ul li {
+    padding: 10px 30px 10px 0;
+  }
+  
+  #nf-header .content nav ul li .btn {
+    background-color: #ffffff;
+    color: #4a4a4a;
+    padding: 6px 16px;
+    border-radius: 40px;
+  }
+  
+  #nf-header .content nav ul li .btn:hover {
+    background-color: #ffffff;
+    color: #ffb900;
+    text-decoration: none;
+  }
+  
+  #nf-header .action {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    padding: 4px;
+    -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+  
+  #nf-header .action img {
+    width: 18px;
+    height: 18px;
+    -o-object-fit: contain;
+       object-fit: contain;
+  }
+  
+  #nf-header-content iframe {
+    width: 100%;
+    height: 100vh;
+    border: none;
+  }
+  
+  #nf-headerCTA {
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
+  
+  #nf-headerCTA img {
+    width: 130px;
+    height: 130px;
+    -o-object-fit: cover;
+       object-fit: cover;
+  }
 </style>
-    <div>
-        <header class="nf-plugin-header">
-            <ul class="links">
-                <li><a class="openpop" href="https://cairo.nowfloats.com/all-our-products/1" id="product">Products</a></li>
-                <li><a class="openpop" href="https://cairo.nowfloats.com/latest-updates/1" id="update">Updates</a></li>
-            </ul>
-        </header>
-        <div class="wrapper">
-            <div class="popup">
-                <iframe src="">
-                    <p>Your browser does not support iframes.</p>
-                </iframe>
-                <a href="#" class="close">X</a>
-            </div>
-        </div>
-    </div>`;
+<header id="nf-header" style="display: none;">
+<div class="content">
+    <div class="logo">
+        <a href="#">
+            <img src="//proj-demo.s-cdn.boostkit.dev/5e96e8a76e0572000109d196/cwd/img/boost.png?v=6" alt="" />
+            Check out our exclusive content
+        </a>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="https://bubblesspa.nowfloats.com/all-services/1" class="openpop">Services</a>
+            </li>
+            <li>
+                <a href="https://bubblesspa.nowfloats.com/memberships" class="openpop">Memberships</a>
+            </li>
+            <li>
+                <a href="https://bubblesspa.nowfloats.com/latest-updates/1" class="openpop">Updates</a>
+            </li>
+            <li>
+                <a href="https://bubblesspa.nowfloats.com/image-gallery/1" class="openpop">Gallery</a>
+            </li>
+            <li>
+                <a href="tel:08042781862" title="Get in contact with us.">08042781862</a>
+            </li>
+            <li>
+                <a href="https://5e9709e9c7627e00015c7b0c.preview.boostkit.dev/reservation" class="openpop btn"
+                    title="Book your appointment">Book Appointment</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+<div class="action">
+    <button class="closeNFIframe" style="display: none;" title="Close Frame">
+        <img src="//proj-demo.s-cdn.boostkit.dev/5e96e8a76e0572000109d196/cwd/img/up.png?v=6" alt="" />
+    </button>
+    <button class="closeNFNav" title="Close Navbar">
+        <img src="//proj-demo.s-cdn.boostkit.dev/5e96e8a76e0572000109d196/cwd/img/close.png?v=6" alt="" />
+    </button>
+</div>
+</header>
+<div id="nf-header-content">
+<iframe id="nf-frame">
+    <p>Your browser does not support iframes.</p>
+</iframe>
+</div>
+<div id="nf-headerCTA" title="Open Navbar">
+<img src="//proj-demo.s-cdn.boostkit.dev/5e96e8a76e0572000109d196/cwd/img/headerCTA.png?v=6" alt="" />
+</div>`;
+  debugger;
   var div = document.createElement("div");
   div.innerHTML = template;
   document.getElementsByTagName("body")[0].prepend(div);
 
-  document.getElementsByClassName("popup")[0].style.display = "none";
+  document.getElementById("nf-header-content").style.display = "none";
   var openlink = document.getElementsByClassName("openpop");
+
+  var NFHeaderCta = document.getElementById('nf-headerCTA');
+  var NFIframeWrapper = document.getElementById('nf-header-content');
+  var NFIframe = document.getElementById('nf-frame');
+  var nfHeader = document.getElementById('nf-header');
+  var btnCloseNfNav = document.getElementsByClassName('closeNFNav')[0];
+  var btnCloseNfFrame = document.getElementsByClassName('closeNFIframe')[0];
+
+  // add listner on every anchor in list
   for (var i = 0; i < openlink.length; i++) {
-    openlink[i].addEventListener("click", function(e) {
+    openlink[i].addEventListener("click", function (e) {
       e.preventDefault();
-      document.getElementsByTagName("iframe")[0].src = this.href;
-      document.getElementsByClassName("popup")[0].style.display = "block";
+      document.getElementById('nf-frame').src  = this.href;
+      document.getElementById("nf-header-content").style.display = "block";
+      btnCloseNfNav.style.display = "none";
+      btnCloseNfFrame.style.display = "block";
     });
   }
 
-  var closeiframe = document.getElementsByClassName("close");
-  closeiframe[0].addEventListener("click", function(e) {
+  // opens header and close cta to open header
+  NFHeaderCta.addEventListener("click", function(e){
     e.preventDefault();
-    document.getElementsByClassName("popup")[0].style.display = "none";
+    nfHeader.style.display = "block";
+    NFHeaderCta.style.display = "none";
   });
+
+  // closes ifram opened
+  btnCloseNfFrame.addEventListener('click',function(e){
+    e.preventDefault();
+    NFIframeWrapper.style.display = "none";
+    btnCloseNfFrame.style.display = "none";
+    btnCloseNfNav.style.display = "block";
+  });
+
+  // closes header & open cta to open header
+  btnCloseNfNav.addEventListener('click', function(e){
+    e.preventDefault();
+    nfHeader.style.display = "none";
+    NFHeaderCta.style.display = "block";
+  })
 }
